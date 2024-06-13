2f5ead787692b3f5d40280779ab9e8407ee5bd2c
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-06-14 04:49:40 +0530

    feat: add tuple initialization for values in app.ts

--- public/app.js
+++ public/app.js
@@ -12,11 +12,13 @@ const list = new LsitTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
